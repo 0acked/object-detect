3442e71a898756ce5bc6835f1ec067d9fcb8d7d7
0acked <98188838+0acked@users.noreply.github.com>
2022-06-19 14:48:54 +0100

    Add files via upload

---
+++ toiletries.js
@@ -0,0 +1,19 @@
+status = "";
+
+function preload(){
+   img = "toiletries.webp";
+}
+function setup(){
+   canvas = createCanvas(300,300);
+   canvas.center();
+   objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+   document.getElementById("status").innerHTML = "Detecting Objects...";
+}
+function modelLoaded(){
+   status = true;
+   console.log("Model loaded.");
+   objectDetector.detect(img,gotResults);
+}
+function gotResults(){
+   console.log(results);
+}
